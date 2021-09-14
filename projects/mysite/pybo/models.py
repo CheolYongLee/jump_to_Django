13a696900f6b7965b3d3f5b2819e62563f0be7cf
이철용 <ypopp1116@naver.com>
2021-09-15 06:54:17 +0900

    #17 작성자 표시 기능 추가

--- projects/mysite/pybo/models.py
+++ projects/mysite/pybo/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=200) # 질문의 제목 (최대 200자)
     content = models.TextField() # 질문의 내용
     # TextField = 글자수 제한 없음
@@ -13,6 +15,7 @@ class Question(models.Model):
         return self.subject
 
 class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # 질문 (어떤 질문의 답변인지 알기 위함), Question 모델을 속성으로 가져감
     # ForeignKey = 기존 모델을 속성으로 연결하기 위함
     # on_delete=models.CASCADE = 이 답변과 연결된 질문(Question)이 삭제 될 경우 답변(Answer)도 함꼐 삭제된다
--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -8,22 +8,23 @@ from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
 
+
 def index(request):
     """
     pybo 목록 출력
     """
     # 입력 파라미터
-    page = request.GET.get('page', '1') # 페이지
+    page = request.GET.get('page', '1')  # 페이지
 
     # 조회
-    question_list = Question.objects.order_by('-create_date') # 작성일시 역순으로 정렬
+    question_list = Question.objects.order_by('-create_date')  # 작성일시 역순으로 정렬
 
     # 페이징처리
-    paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여주기
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
 
     context = {'question_list': page_obj}
-    return render(request, 'pybo/question_list.html', context) # 파이썬 데이터를 템플릿에 적용하여 HTML로 반환
+    return render(request, 'pybo/question_list.html', context)  # 파이썬 데이터를 템플릿에 적용하여 HTML로 반환
 
 def detail(request, question_id):
     """
@@ -34,6 +35,7 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
 
+
 def answer_create(request, question_id):
     """
     pybo 답변등록
@@ -43,6 +45,7 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # author 속성에 로그인 계정 저장
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -52,6 +55,7 @@ def answer_create(request, question_id):
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
 
+
 def question_create(request):
     """
     pybo 질문등록
@@ -60,6 +64,7 @@ def question_create(request):
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user  # author 속성에 로그인 계정 저장
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
